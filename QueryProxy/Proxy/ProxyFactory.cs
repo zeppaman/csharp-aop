11530385c8e127bc4e1721f8611ac1166cd38e56
zeppaman <zeppaman@altervista.org>
2020-03-01 12:02:46 +0100

    fix formatting

--- QueryProxy/Proxy/ProxyFactory.cs
+++ QueryProxy/Proxy/ProxyFactory.cs
@@ -9,45 +9,40 @@ namespace QueryProxy.Proxy
 {
     public class ProxyFacotory<T> : DispatchProxy
     {
+        private T _decorated;
+        private T _interface;
 
-            private T _decorated;
-            private T _interface;
-
-            protected override object Invoke(MethodInfo targetMethod, object[] args)
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            var interfaceMethods = _decorated.GetType().GetMethods();
+            foreach (var method in interfaceMethods)
             {
-               var interfaceMethods = _decorated.GetType().GetMethods();
-               foreach (var method in interfaceMethods)
-               {
-                  Debug.WriteLine("method" + method.Name);
-               }
+                Debug.WriteLine("method" + method.Name);
+            }
 
             var annotations = targetMethod.GetCustomAttributes(true);
-            var aopAttr= annotations.FirstOrDefault(x => typeof(AOPAttribute).IsAssignableFrom(x.GetType())) as AOPAttribute;
+            var aopAttr = annotations.FirstOrDefault(x => typeof(AOPAttribute).IsAssignableFrom(x.GetType())) as AOPAttribute;
 
             if (aopAttr != null)
             {
                 return aopAttr.Execute(targetMethod, args, annotations);
             }
 
+            var result = targetMethod.Invoke(_decorated, args);
+            return result;
+        }
 
+        public static T Create<T>() where T : class
+        {
+            object proxy = Create<T, ProxyFacotory<T>>();
+            ((ProxyFacotory<T>)proxy).SetParameters(proxy as T);
 
-                var result = targetMethod.Invoke(_decorated, args);
-                return result;
-            }
+            return (T)proxy;
+        }
 
-            public static T Create<T>() where T:class
-            {
-                object proxy = Create<T, ProxyFacotory<T>>();
-                ((ProxyFacotory<T>)proxy).SetParameters(proxy as T);
-
-                return (T)proxy;
-            }
-
-            private  void SetParameters(T decorated)
-            {
-             _decorated = decorated;
-            }
-        
-
+        private void SetParameters(T decorated)
+        {
+            _decorated = decorated;
+        }
     }
 }
